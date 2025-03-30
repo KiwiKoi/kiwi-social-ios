d68e617ac61360f1c023e8d401f1dede4f1bcc9d
Daniel <danielo.visage@gmail.com>
2025-03-30 18:20:29 +0200

    Initial Commit

---
+++ KiwiSocial/KiwiSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiwiSocialApp.swift
+//  KiwiSocial
+//
+//  Created by Daniel Visage on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct KiwiSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KiwiSocialTests/KiwiSocialTests.swift
@@ -0,0 +1,17 @@
+//
+//  KiwiSocialTests.swift
+//  KiwiSocialTests
+//
+//  Created by Daniel Visage on 30/03/2025.
+//
+
+import Testing
+@testable import KiwiSocial
+
+struct KiwiSocialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
